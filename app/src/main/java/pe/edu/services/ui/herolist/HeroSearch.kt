b59aa1eb40378ec06b564a9894513919f89ab787
TypeScriptReview <u20201b782@upc.edu.pe>
2023-09-05 15:54:01 -0500

    (feat) search options config

--- app/src/main/java/pe/edu/services/ui/herolist/HeroSearch.kt
+++ app/src/main/java/pe/edu/services/ui/herolist/HeroSearch.kt
@@ -1,5 +1,6 @@
 package pe.edu.services.ui.herolist
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
@@ -9,11 +10,14 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import data.model.Hero
+import data.repository.HeroRepository
 
 
 @Composable
@@ -27,17 +31,17 @@ fun HeroSearch(){
         mutableStateOf(listOf<Hero>())
     }
     Column{
-        Search()
+        Search(textQuery, heroes)
         HeroList(heroes)
     }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Search() {
-    val textQuery = remember {
-        mutableStateOf("")
-    }
+fun Search(textQuery: MutableState<String>, heroes: MutableState<List<Hero>>) {
+
+    val repository = HeroRepository()
+    val context = LocalContext.current
 
     OutlinedTextField(value = textQuery.value, onValueChange = { newTextQuery ->
         textQuery.value = newTextQuery },
@@ -50,6 +54,13 @@ fun Search() {
         ),
         keyboardActions = KeyboardActions(
             onSearch = {
+                repository.searchByName(textQuery.value) {result ->
+                    if(result is Result.Success) {
+                        heroes.value = result.data!!
+                    }else {
+                        Toast.makeText(context, result.message!!, Toast.LENGTH_SHORT).show()
+                    }
+                }
 
 
             }
